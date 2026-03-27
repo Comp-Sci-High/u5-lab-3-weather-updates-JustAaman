3a4b5c2bf9df58d507d7b61e66ce62c50f5ac1a9
JustAaman <162189639+JustAaman@users.noreply.github.com>
2026-03-27 19:57:09 +0000

    Weather Report

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // 1. import useState from React
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   // 2. Create weather state with default "Sunny"
-
+const[weather, setWeather] = useState("Sunny")
 
 
   // 3. import useEffect from React
@@ -13,28 +13,45 @@ function App() {
 
   // 4. Create handleRainy function
   // change weather to "Rainy"
-
+const handleRainy = () => {
+  setWeather("Rainy")
+};
 
 
   // 5. Create handleSnowy function
   // change weather to "Snowy"
-
+const handleSnowy = () => {
+  setWeather("Snowy")
+};
 
 
   // 6. Create handleHot function
   // change weather to "Hot"
-
+const handleHot = () => {
+  setWeather("Hot")
+};
 
 
   // 7. Create handleReset function
   // change weather back to "Sunny"
-
+const handleReset = () => {
+    setWeather("Sunny");
+  };
 
 
   // 8. Add useEffect that runs when weather changes
   // show alerts based on weather value
-
-
+useEffect(() => {
+    if (weather === "Rainy") {
+      alert("Take an umbrella.");
+    } else if (weather === "Snowy") {
+      alert("Wear a coat.");
+    } else if (weather === "Hot") {
+      alert("Drink water.");
+    } else if (weather === "Sunny") {
+      alert("Enjoy the sunshine.");
+    }
+  }, [weather]); 
 
   return (
     <div className="page">
@@ -48,32 +65,32 @@ function App() {
           <h2>Current Weather</h2>
 
           {/* 9. Display weather here */}
-          <p className="output">Weather: </p>
+          <p className="output">Weather: {weather} </p>
 
           <button
             className="btn"
-            // 10. connect to handleRainy
+            onClick={handleRainy}// 10. connect to handleRainy
           >
             Rainy 🌧️
           </button>
 
           <button
             className="btn"
-            // 11. connect to handleSnowy
+            onClick={handleSnowy}// 11. connect to handleSnowy
           >
             Snowy ❄️
           </button>
 
           <button
             className="btn"
-            // 12. connect to handleHot
+            onClick={handleHot}// 12. connect to handleHot
           >
             Hot ☀️
           </button>
 
           <button
             className="btn secondary"
-            // 13. connect to handleReset
+            onClick={handleReset}// 13. connect to handleReset
           >
             Reset
           </button>
